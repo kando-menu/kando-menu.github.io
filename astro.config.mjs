e658014f45fb7a46e3db26e1b4675f8d801754e4
Simon Schneegans <code@simonschneegans.de>
2024-11-19 21:15:41 +0100

    :tada: Add some initial content

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,21 +8,35 @@ export default defineConfig({
   integrations: [
     starlight({
       title: 'Kando',
+      logo: {
+        src: './src/assets/kando.svg',
+        alt: 'Kando Logo',
+      },
+      editLink: {
+        baseUrl:
+            'https://github.com/kando-menu/kando-menu.github.io/edit/main/',
+      },
       social: {
         github: 'https://github.com/kando-menu/kando',
+        discord: 'https://discord.gg/hZwbVSDkhy',
+        youtube: 'https://www.youtube.com/@simonschneegans',
       },
       sidebar: [
         {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            {label: 'Example Guide', slug: 'guides/example'},
-          ],
+          label: 'Getting Started',
+          autogenerate: {directory: 'getting-started'},
         },
         {
           label: 'Reference',
           autogenerate: {directory: 'reference'},
         },
+        {label: 'Changelog', link: '/changelog'},
+        {label: 'Code of Conduct', link: '/code-of-conduct'},
+      ],
+      lastUpdated: true,
+      customCss: [
+        // Relative path to your custom CSS file
+        './src/styles/custom.css',
       ],
     }),
   ],
